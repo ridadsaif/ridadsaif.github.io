1fcae6455c4a685ec642346eaa7e62ce0a816f66
ridadsaif <ridadsaif@yahoo.com>
2025-03-23 18:32:58 -0400

    Create constants.py

---
+++ dynamic_interest_profile/constants.py
@@ -0,0 +1,10 @@
+
+# Configuration Parameters
+SIM_MODELS = ["model1", "model2"]
+RELEVANCE_MODELS = ["model1", "model2"]
+LIMIT_KEYWORDS = 10
+ALPHA = 0.6  # Balance between old and new profile data
+
+# MongoDB Secret IDs
+DB_SECRET_ID = "DB_SECRET"
+DB_SECRET_ID_USER_DATA = "DB_SECRET_USER_DATA"
